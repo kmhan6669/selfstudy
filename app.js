73e120df4cddab1c133eb3c0c2f071e3fe8557b3
kmhan6669 <118246701+kmhan6669@users.noreply.github.com>
2023-02-27 11:34:49 +0900

    81. done

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-const http = require('http');
+const path = require('path')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+const adminData = require('./routes/admin');
+const shopRouter = require('./routes/shop');
 
+const app = express();
+
+app.set('view engine', 'pug')
+app.set('views', 'views')
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminData.routes);
+
+app.use(shopRouter);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 });
 
-server.listen(3000);
+
+app.listen(3000, function () {
+    console.log(`server listens 3000....`)
+});
